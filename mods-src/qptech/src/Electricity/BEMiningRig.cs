4a6386b2f52736e4d5d321c0464f082fafe33a53
Quentin Preik <quentin@preik.net>
2022-11-29 06:45:42 -0500

    Able to read ore distributions (in BEMiningRig - just need to do all the other things now)

--- mods-src/qptech/src/Electricity/BEMiningRig.cs
+++ mods-src/qptech/src/Electricity/BEMiningRig.cs
@@ -10,6 +10,8 @@ using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 using Vintagestory.API.Client;
 using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+using Vintagestory.ServerMods;
 using HarmonyLib;
 
 
@@ -22,10 +24,17 @@ namespace qptech.src.Electricity
         int skipcounter;
         public virtual int skip => 5;
         public virtual int range => 1;
+        ProPickWorkSpace ppws;
+        bool temp = false;
         public override void Initialize(ICoreAPI api)
         {
             base.Initialize(api);
-            
+            ppws = ObjectCacheUtil.GetOrCreate<ProPickWorkSpace>(api, "propickworkspace", () =>
+            {
+                ProPickWorkSpace ppws = new ProPickWorkSpace();
+                ppws.OnLoaded(api);
+                return ppws;
+            });
         }
 
         public override void OnTick(float par)
@@ -49,6 +58,7 @@ namespace qptech.src.Electricity
                     Api.World.BlockAccessor.SetBlock(0, drillpos);
                 }
                 drillpos.X++;
+                if (!temp) { DoSurvey(Api.World, Pos); }
                 if ((drillpos.X - Pos.X) > range)
                 {
                     drillpos.X = Pos.X - range;
@@ -65,7 +75,62 @@ namespace qptech.src.Electricity
         }
 
         BlockPos StartDrillPos=>new BlockPos(Pos.X-range, Pos.Y + drillstartyoffset, Pos.Z-range);
+
+        protected virtual void DoSurvey( IWorldAccessor world,  BlockPos pos)
+        {
+            temp = true;
             
+            
+            DepositVariant[] deposits = Api.ModLoader.GetModSystem<GenDeposits>()?.Deposits;
+            if (deposits == null) return;
+
+            IBlockAccessor blockAccess = world.BlockAccessor;
+            int chunksize = blockAccess.ChunkSize;
+            int regsize = blockAccess.RegionSize;
+
+            IMapRegion reg = world.BlockAccessor.GetMapRegion(pos.X / regsize, pos.Z / regsize);
+            int lx = pos.X % regsize;
+            int lz = pos.Z % regsize;
+
+            pos = pos.Copy();
+            pos.Y = world.BlockAccessor.GetTerrainMapheightAt(pos);
+
+            int[] blockColumn = ppws.GetRockColumn(pos.X, pos.Z);
+
+            List<KeyValuePair<double, string>> readouts = new List<KeyValuePair<double, string>>();
+
+            List<string> traceamounts = new List<string>();
+
+            foreach (var val in reg.OreMaps)
+            {
+                IntDataMap2D map = val.Value;
+                int noiseSize = map.InnerSize;
+
+                float posXInRegionOre = (float)lx / regsize * noiseSize;
+                float posZInRegionOre = (float)lz / regsize * noiseSize;
+
+                int oreDist = map.GetUnpaddedColorLerped(posXInRegionOre, posZInRegionOre);
+
+                double ppt;
+                double totalFactor;
+
+                if (!ppws.depositsByCode.ContainsKey(val.Key))
+                {
+                    
+                    continue;
+                }
+
+                ppws.depositsByCode[val.Key].GetPropickReading(pos, oreDist, blockColumn, out ppt, out totalFactor);
+                
+                if (totalFactor > 0.002)
+                {
+                    readouts.Add(new KeyValuePair<double, string>(totalFactor, val.Key));
+                }
+            }
+            bool x = true;
+            
+        }
+
 
         public override void ToTreeAttributes(ITreeAttribute tree)
         {
